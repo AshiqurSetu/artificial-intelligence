b885fb35294e2e62807e43a80347d7ea371dda0a
ashiqsetu <setu037@gmail.com>
2019-09-26 02:14:27 +0600

    Worked

---
+++ script.js
@@ -0,0 +1,40 @@
+
+var mainForm = document.getElementById("mainForm");
+var textField = document.getElementById("textField");
+var askButton = document.getElementById("askButton");
+var charArea = document.getElementById("charArea");
+
+mainForm.addEventListener('submit', run, false);
+
+function run() {
+	alert("Connected");
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
